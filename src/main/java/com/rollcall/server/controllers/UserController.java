0b80745b39e77d8cc81032e3d99d5fb7ad6a205e
kunal <13kunalthakur@gmail.com>
2024-04-12 14:16:26 +0530

    added cross origin for handling cors erros

--- src/main/java/com/rollcall/server/controllers/UserController.java
+++ src/main/java/com/rollcall/server/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/user")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
