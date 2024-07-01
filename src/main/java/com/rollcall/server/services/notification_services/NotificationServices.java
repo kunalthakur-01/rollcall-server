144b98f095179925afa6d50c8f60748ab42907b7
kunal <13kunalthakur@gmail.com>
2024-07-01 13:54:40 +0530

    notification services interface

--- src/main/java/com/rollcall/server/services/notification_services/NotificationServices.java
+++ src/main/java/com/rollcall/server/services/notification_services/NotificationServices.java
@@ -3,11 +3,8 @@ package com.rollcall.server.services.notification_services;
 import java.util.List;
 import java.util.UUID;
 
-import com.rollcall.server.models.StudentNotification;
-import com.rollcall.server.models.TeacherNotification;
+import com.rollcall.server.models.Notification;
 
 public interface NotificationServices {
-    List<StudentNotification> getStudentNotifications(UUID userId);
-
-    List<TeacherNotification> getTeacherNotifications(UUID userId);
+    List<Notification> getNotifications(UUID userId);
 }
